8c6462fdf68a07766e9816386685c2111fe2a824
kebinoffice <kebin@smartlemon.cn>
2017-08-04 12:31:43 +0800

    Home page

--- src/components/page/Home.vue
+++ src/components/page/Home.vue
@@ -1,6 +1,15 @@
 <template>
   	<div class="body">
-  		<flexbox :gutter="0">
+  		<flexbox justify="center">
+  			<flexbox-item :span="12">
+  				<div class="top-item">
+					 <swiper loop auto dots-position="center" class="wapper">
+					 	<swiper-item v-for="(item, index) in wipper_list" :key="index"><img :src="item" class="wapper"></swiper-item>
+					 </swiper>
+				</div>
+  			</flexbox-item>
+  		</flexbox>
+  		<flexbox justify="center">
 			<flexbox-item :span="4">
 				<div class="item">
 					<span>fuck</span>
@@ -10,24 +19,24 @@
 				<div class="item">hello</div>
 			</flexbox-item>
 	    </flexbox>
-	    <flexbox :gutter="0">
+	    <flexbox justify="center">
 			<flexbox-item :span="4">
 				<div class="item">
 					<span>fuck</span>
 				</div>
 			</flexbox-item>
-			<flexbox-item :span="4">
+			<flexbox-item :span="3.71">
 				<div class="item">
 					<span>fuck</span>
 				</div>
 			</flexbox-item>
-			<flexbox-item :span="4">
+			<flexbox-item :span="3.71">
 				<div class="item">
 					<span>fuck</span>
 				</div>
 			</flexbox-item>
 	    </flexbox>
-	    <flexbox :gutter="0">
+	    <flexbox justify="center">
 			<flexbox-item :span="4">
 				<div class="item">
 					<span>fuck</span>
@@ -37,39 +46,39 @@
 				<div class="item">hello</div>
 			</flexbox-item>
 	    </flexbox>
-	    <flexbox :gutter="0">
+	    <flexbox justify="center">
 			<flexbox-item :span="4">
 				<div class="item">
 					<span>fuck</span>
 				</div>
 			</flexbox-item>
-			<flexbox-item :span="4">
-				<flexbox :gutter="0">
-					<flexbox-item :span="6">
+			<flexbox-item :span="3.8">
+				<flexbox justify="center">
+					<flexbox-item :span="5.65">
 						<div class="item-item">
 							<span>fuck</span>
 						</div>
 					</flexbox-item>
-					<flexbox-item :span="6">
+					<flexbox-item :span="5.65">
 						<div class="item-item">
 							<span>fuck</span>
 						</div>
 					</flexbox-item>
 				</flexbox>
-				<flexbox :gutter="0">
-					<flexbox-item :span="6">
+				<flexbox justify="center">
+					<flexbox-item :span="5.65">
 						<div class="item-item">
 							<span>fuck</span>
 						</div>
 					</flexbox-item>
-					<flexbox-item :span="6">
+					<flexbox-item :span="5.65">
 						<div class="item-item">
 							<span>fuck</span>
 						</div>
 					</flexbox-item>
 				</flexbox>
 			</flexbox-item>
-			<flexbox-item :span="4">
+			<flexbox-item :span="3.7">
 				<div class="item">
 					<span>fuck</span>
 				</div>
@@ -79,12 +88,21 @@
 </template>
 
 <script>
-import { Flexbox, FlexboxItem } from 'vux'
+import { Flexbox, FlexboxItem, Swiper, SwiperItem } from 'vux'
 
 export default {
 	components: {
 		Flexbox,
-		FlexboxItem
+		FlexboxItem,
+		Swiper, SwiperItem
+	},
+	data: function() {
+		return {
+			wipper_list: [
+				'http://img.sccnn.com/bimg/337/40711.jpg',
+				'http://pic.qiantucdn.com/58pic/18/48/19/5627b4a0ac2b8_1024.jpg'
+			]
+		}
 	}
 }
 </script>
@@ -92,17 +110,32 @@ export default {
 <style>
 .body {
 	background-color: black;
-} 
+	padding: .5rem .5rem 0 .5rem;
+	height: 100%;
+}
+
+.top-item {
+	height: 12rem;
+	background-color: #fff;
+	text-align: center;
+	margin-bottom: .5rem;
+}
+
+.wapper {
+	height: 9rem
+}
 
 .item {
+	height: 9rem;
 	background-color: #fff;
-	height: 160px;
 	text-align: center;
+	margin-bottom: .5rem;
 }
 
 .item-item {
 	background-color: #fff;
-	height: 80px;
+	height: 4.2rem;
 	text-align: center;
+	margin-bottom: .5rem;
 }
 </style>
